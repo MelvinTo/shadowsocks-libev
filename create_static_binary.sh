7e4e678cf4664c972659f51ab46fdb7bc575f820
Melvin Tu <melvinto@gmail.com>
2017-04-06 19:14:17 +0000

    add script to build static verison

---
+++ create_static_binary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMMON_OBJ_FILES=$(ls -1 slibs/*)
+LINK_LIBS="-Wall -lev -ludns  -lsodium -lpcre -lm -lpcre -lpthread"
+
+gcc -o fw_ss_client src/ss_local*.o $COMMON_OBJ_FILES $LINK_LIBS -static 
+gcc -o fw_ss_tunnel src/ss_tunnel*.o $COMMON_OBJ_FILES $LINK_LIBS -static 
+gcc -o fw_ss_redir src/ss_redir*.o $COMMON_OBJ_FILES $LINK_LIBS -static 
