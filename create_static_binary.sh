ec4724dfc3fc2ed1d7a31d853c84bdf7d8b64fc4
Melvin Tu <melvinto@gmail.com>
2017-07-08 09:35:55 +0000

    build ss_server too

--- create_static_binary.sh
+++ create_static_binary.sh
@@ -6,3 +6,4 @@ LINK_LIBS="-Wall -lev -ludns  -lsodium -lpcre -lm -lpcre -lpthread"
 gcc -o fw_ss_client src/ss_local*.o $COMMON_OBJ_FILES $LINK_LIBS -static 
 gcc -o fw_ss_tunnel src/ss_tunnel*.o $COMMON_OBJ_FILES $LINK_LIBS -static 
 gcc -o fw_ss_redir src/ss_redir*.o $COMMON_OBJ_FILES $LINK_LIBS -static 
+gcc -o fw_ss_server src/ss_server*.o $COMMON_OBJ_FILES $LINK_LIBS -static 
